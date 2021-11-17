a73e96d9cbffbf7e47b6de6efacc30856c0304dc
mb925 <martina.bevilacqua5>
2021-11-17 23:44:05 +0100

    automatic drive

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
 # This is a sample Python script.
 import os
+from pathlib import Path
 
 import cv2
 import matplotlib.pyplot as plt
 import numpy as np
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import time
 
 
 def resizeImage(image):
@@ -69,9 +74,99 @@ def img2sketch(photo, k_size):
 
 def rename():
     input = '/home/martina/PycharmProjects/sketch/input'
+    rename = '/home/martina/PycharmProjects/sketch/rename'
     files = os.listdir(input)
     for index, file in enumerate(files):
-        os.rename(os.path.join(input, file), os.path.join(input, f'Human Face #{index+1}.jpg'))
+        os.rename(os.path.join(input, file), os.path.join(rename, f'Human Face #{index+1}.jpg'))
+
+
+def metamask(driver):
+
+    SECRET_RECOVERY_PHRASE = ''
+    NEW_PASSWORD = ''
+
+
+    time.sleep(2)
+
+    css = '#app-content > div > div.main-container-wrapper > div > div > div > button'
+    driver.find_element(By.CSS_SELECTOR, css).click()
+    time.sleep(2)
+
+    css = '#app-content > div > div.main-container-wrapper > div > div > div.select-action__wrapper > div > div.select-action__select-buttons > div:nth-child(1) > button'
+    driver.find_element(By.CSS_SELECTOR, css).click()
+    time.sleep(2)
+
+    css = '#app-content > div > div.main-container-wrapper > div > div > div > div.metametrics-opt-in__footer > div.page-container__footer > footer > button.button.btn--rounded.btn-primary.page-container__footer-button'
+    driver.find_element(By.CSS_SELECTOR, css).click()
+    time.sleep(2)
+
+    css = '#app-content > div > div.main-container-wrapper > div > div > form > div.first-time-flow__textarea-wrapper > div.MuiFormControl-root.MuiTextField-root.first-time-flow__textarea.first-time-flow__seedphrase > div > input'
+    driver.find_element(By.CSS_SELECTOR, css).send_keys(SECRET_RECOVERY_PHRASE)
+    time.sleep(2)
+
+    css = '#password'
+    driver.find_element(By.CSS_SELECTOR, css).send_keys(NEW_PASSWORD)
+    time.sleep(2)
+
+    css = '#confirm-password'
+    driver.find_element(By.CSS_SELECTOR, css).send_keys(NEW_PASSWORD)
+    time.sleep(2)
+
+    css = '#app-content > div > div.main-container-wrapper > div > div > form > div.first-time-flow__checkbox-container > div'
+    driver.find_element(By.CSS_SELECTOR, css).click()
+    time.sleep(2)
+
+    css = '#app-content > div > div.main-container-wrapper > div > div > form > button'
+    driver.find_element(By.CSS_SELECTOR, css).click()
+    time.sleep(2)
+
+    css = '#app-content > div > div.main-container-wrapper > div > div > button'
+    driver.find_element(By.CSS_SELECTOR, css).click()
+    time.sleep(2)
+
+def driver_exec():
+    print(Path.home())
+    EXTENSION_PATH = '/home/martina/.config/google-chrome/Default/Extensions/nkbihfbeogaeaoehlefnkodbefgpgknn/10.5.2_0.crx'
+    DRIVER_PATH = '/home/martina/PycharmProjects/sketch/chromedriver'
+    print(EXTENSION_PATH)
+
+    opt = webdriver.ChromeOptions()
+    opt.add_extension(EXTENSION_PATH)
+    driver = webdriver.Chrome(executable_path=DRIVER_PATH, options=opt)
+
+    driver.switch_to.window(driver.window_handles[0])
+    metamask(driver)
+    driver.switch_to.window(driver.window_handles[-1])
+
+    driver.get('https://opensea.io/collection/face-of-humanity/assets/create')
+    time.sleep(5)
+
+    # print(driver.window_handles)
+
+    css = '#__next > div.Blockreact__Block-sc-1xf18x6-0.Flexreact__Flex-sc-1twd32i-0.FlexColumnreact__FlexColumn-sc-1wwz3hp-0.OpenSeaPagereact__DivContainer-sc-65pnmt-0.dBFmez.jYqxGr.ksFzlZ.fiudwD > main > div > div > div > div.Blockreact__Block-sc-1xf18x6-0.eOSaGo > ul > li:nth-child(1) > button > div.Blockreact__Block-sc-1xf18x6-0.Flexreact__Flex-sc-1twd32i-0.FlexColumnreact__FlexColumn-sc-1wwz3hp-0.VerticalAlignedreact__VerticalAligned-sc-b4hiel-0.Itemreact__ItemContent-sc-1idymv7-1.dBFmez.jYqxGr.ksFzlZ.iXcsEj.hTefVc'
+    driver.find_element(By.CSS_SELECTOR, css).click()
+    time.sleep(2)
+
+    driver.switch_to.window(driver.window_handles[-1])
+
+    css = '#app-content > div > div.main-container-wrapper > div > div.permissions-connect-choose-account > div.permissions-connect-choose-account__footer-container > div.permissions-connect-choose-account__bottom-buttons > button.button.btn--rounded.btn-primary'
+    driver.find_element(By.CSS_SELECTOR, css).click()
+    time.sleep(2)
+
+    css = '#app-content > div > div.main-container-wrapper > div > div.page-container.permission-approval-container > div.permission-approval-container__footers > div.page-container__footer > footer > button.button.btn--rounded.btn-primary.page-container__footer-button'
+    driver.find_element(By.CSS_SELECTOR, css).click()
+    time.sleep(5)
+
+    driver.switch_to.window(driver.window_handles[-1])
+
+    css = '#app-content > div > div.main-container-wrapper > div > div.request-signature__footer > button.button.btn--rounded.btn-primary.btn--large.request-signature__footer__sign-button'
+    driver.find_element(By.CSS_SELECTOR, css).click()
+    time.sleep(5)
+
+    # print(driver.window_handles)
+
+    # driver.close()
+
 
 
 # Press the green button in the gutter to run the script.
@@ -79,16 +174,18 @@ if __name__ == '__main__':
     # Function call
     rename()
 
-    # for filename in os.listdir('input'):
-    #     print(filename)
-    #
-    #     image = cv2.imread(f'input/{filename}')
-    #     resized_image = resizeImage(image)
-    #     coloured = ColorQuantization(resized_image)
-    #     contoured = findCountours(coloured)
-    #     final_image = contoured
-    #     cv2.imwrite('output_640x860/' + filename, final_image)
-    #     print("Image saved!")
+    for filename in os.listdir('rename'):
+        print(filename)
+
+        image = cv2.imread(f'rename/{filename}')
+        resized_image = resizeImage(image)
+        coloured = ColorQuantization(resized_image)
+        contoured = findCountours(coloured)
+        final_image = contoured
+        cv2.imwrite('output_640x860/' + filename, final_image)
+        print("Image saved!")
+
+    # driver_exec()
 
 
 
